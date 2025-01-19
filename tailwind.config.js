1981dabe96f88a5114040c71ec58d9e3196b7794
alvaro neto <alvaronetobsb@gmail.com>
2025-01-19 11:48:06 -0300

    Instalação com sucesso do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bronze: "#c4a484",
+        "bronze-dark": "#8b4513",
+        "custom-brown": "#4a2511",
+      },
+    },
+  },
+  plugins: [],
+};
